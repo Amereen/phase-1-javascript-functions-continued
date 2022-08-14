f73f66702c45f9b1fe9b7db3ede11719b3c93acf
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-08-14 17:04:48 -0400

    commit tree a4bb5685ff06af3e27185783a8df307a3e0af312

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate") {
+  return (`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork= function(activity="go to the office"){
+  return (`This Monday, I will ${activity}.`)
+
+}
+
+
+  // function wrapAdjective
+
+
+
+
+
+
+
+
+
+
+
+function wrapAdjective (whatever="*") {
+  return function (inner="special"){
+    const pt1= (`You are ${whatever}${inner}${whatever}!`)
+      return (pt1)
+  }
+  return function(){
+    
+  }
+  
+
+}
